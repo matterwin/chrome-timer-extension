d7af942131c62c2b1add4933db36ee55671cf9f3
matterwin <matthewderwin23@gmail.com>
2024-08-04 00:13:16 -0500

    pushing

--- App/extension/public/firebaseConfig.js
+++ App/extension/public/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/fireba
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMxF0lP0MqZuXIvEWE8RIDYX1DLb-Uows",
-  authDomain: "timer-734bc.firebaseapp.com",
-  projectId: "timer-734bc",
-  storageBucket: "timer-734bc.appspot.com",
-  messagingSenderId: "190169259119",
-  appId: "1:190169259119:web:8dc2d28d2a78ec5a4c419a"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
